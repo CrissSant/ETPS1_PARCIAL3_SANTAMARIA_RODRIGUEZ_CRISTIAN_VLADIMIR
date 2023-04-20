fee83c0d7e7554803cc61593aa7f9ca7c80a892e
Cristian Santamaria <124642195+CrissSant@users.noreply.github.com>
2023-04-19 20:41:25 -0600

    P

---
+++ ETPS1_PARCIAL3_BY_SANTAMARIA/app/src/main/java/sv/edu/utec/etps1_parcial3_by_santamaria/Clases/EliminarFragment.java
@@ -0,0 +1,24 @@
+package sv.edu.utec.etps1_parcial3_by_santamaria.Clases;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sv.edu.utec.etps1_parcial3_by_santamaria.R;
+
+
+public class EliminarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_eliminar, container, false);
+    }
+}
